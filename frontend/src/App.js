d88909c6021704cfd39517f07eae7e074bf0ede3
Klimuk Dzmitry <dklimuk@exadel.com>
2020-12-11 20:09:22 +0300

    rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Fotter from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello ProShop</h1>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <Container classNmae="py-3">
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Fotter />
+        </>
+    )
 }
 
-export default App;
+export default App
